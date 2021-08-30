a3c76629d43e411bbd6b74d01717c5d98dc2aa93
Carlos Godri <cggodri@gmail.com>
2021-08-29 22:23:04 -0300

    [chore] configurado clipboard

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,11 @@
 import { Header } from '@components/Header';
 import { Search } from '@components/Search';
 import React, { useState, useEffect, useCallback } from 'react';
-
+import { useClipboard } from '@react-native-clipboard/clipboard';
+import { useIsFocused } from '@react-navigation/native';
 import ShareMenu, { ShareCallback } from 'react-native-share-menu';
 import { Container } from './styles';
+import { useStorage } from '@hooks/storage';
 
 type SharedItem = {
   mimeType: string;
@@ -11,7 +13,21 @@ type SharedItem = {
   extraData: any;
 };
 export function Home() {
-  const [sharedData, setSharedData] = useState<string | null>(null);
+  const isFocused = useIsFocused();
+  const { validUrl } = useStorage();
+  const [clipboard] = useClipboard();
+
+  useEffect(() => {
+    function checkClipboard() {
+      if (validUrl(clipboard)) {
+        alert(`${clipboard} é uma url para salvar?`);
+      }
+    }
+
+    if (isFocused) {
+      checkClipboard();
+    }
+  }, [clipboard, validUrl, isFocused]);
 
   const handleShare = useCallback<ShareCallback>(item => {
     if (!item) {
@@ -19,8 +35,6 @@ export function Home() {
     }
 
     const { data } = item;
-
-    setSharedData(data);
   }, []);
 
   useEffect(() => {
